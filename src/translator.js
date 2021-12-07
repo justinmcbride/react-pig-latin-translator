f756107ec97bbaef04d8579500f2d9d1495860c1
unknown <kwebber@mersive.com>
2021-12-07 14:20:52 -0700

    updated for words that use y as vowel

--- src/translator.js
+++ src/translator.js
@@ -1,19 +1,35 @@
-
-
 function translator(english) {
     const vowel = {a:'a', e:'e', i:'i', o:'o', u:'u'}
 
     let index= 0
+    let hasVowel = 0
     for (index; index < english.length; index++) {
         if (english[index] in vowel) {
+            hasVowel = 1
             break;
-        }  
+        }
     }
-
+    if (hasVowel == 0) {
+      let index2 = 0
+      for (index2; index2 < english.length; index2++) {
+        if (english[index2] == 'y') {
+          index=index2
+          break
+        }
+      }
+    }
     const consonants = english.replace(english.substr(index), 'ay')
     const restOfWord = english.substr(index)
     const pigLatin = restOfWord.concat('', consonants)
-    console.log(pigLatin)
+    return pigLatin
 }
 
-console.log(translator('cheese'))
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Enter an English word or phrase: ', word => {
+  console.log("Pig Latin:", translator(word));
+  readline.close();
+});

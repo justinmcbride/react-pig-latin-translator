cae35dc6f587f6fac6363388e64f38ba3e5247fe
Justin McBride <justin@mcbride.io>
2021-12-08 00:29:29 -0700

    variable renaming; css cleanup; logo cleanup

--- src/App.js
+++ src/App.js
@@ -1,52 +1,71 @@
-import logo from './pigggggy.png';
 import './App.css'
 import { useState } from 'react'
 import translator from './translator.js'
 
 function App() {
-  const [word, setWord] = useState('');
-  const [anotherWord, setAnotherWord] = useState([])
+  const [englishInput, setEnglishInput] = useState('');
+  const [pigLatinOutput, setPigLatinOutput] = useState('Type some English below!');
+
+  const [pigSpinSpeed, setPigSpinSpeed] = useState(20);
+
   const handleChange = (e) => {
-    const allText = e.target.value;
-    if (allText[allText.length-1] === ' ') {
-      console.log("short circuit");
+    const currentInputText = e.target.value;
+    if (currentInputText[currentInputText.length-1] === ' ') {
+      // if the last character entered was a space,
+      // we don't want to create a loop, so we return early.
       return;
-
     }
-    console.log(`handleChange: allText=[${allText}]`);
-    setWord(allText);
+
+    console.log(`handleChange: currentInputText=[${currentInputText}]`);
+    setEnglishInput(currentInputText);
   }
-  const handleSpace= (e) => {
-    if (e.keyCode === 32) {
-      const wordArray = word.split(' ')
-      let allTranslatedWords = ''
 
-      for( const word of wordArray ) {
-        const translatedWord = translator(word);
-        allTranslatedWords += translatedWord + " ";
-      }
-
-      console.log(`handleSpace: originalword=[${word}] allTranslatedWords=[${allTranslatedWords}]`);
-      setAnotherWord(allTranslatedWords);
+  const handleSpace = (e) => {
+    if (e.keyCode !== 32) {
+      // we only care about spaces, so just exit if not a space.
+      return;
     }
+
+    // tokenize the words off the space
+    const wordArray = englishInput.split(' ');
+
+    // create a variable to store the concatenated output
+    let allTranslatedWords = '';
+
+    // iterate over every individual word
+    for (const word of wordArray) {
+      // perform the translation of the individual word
+      const translatedWord = translator(word);
+
+      // add it to the final output
+      allTranslatedWords += translatedWord + " ";
+    }
+
+    console.log(`handleSpace: englishInput=[${englishInput}] allTranslatedWords=[${allTranslatedWords}]`);
+    setPigLatinOutput(allTranslatedWords);
+
+    setPigSpinSpeed(pigSpinSpeed-1);
+    console.log(`pigSpinSpeed=[${pigSpinSpeed}]`);
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-      <p>
-          Pig Latin Translator
-      </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <span className="box2" id='display'> {anotherWord} </span>
-        <input
-          className="box"
-          type="text"
-          onChange={handleChange}
-          onKeyDown={handleSpace}
-          placeholder="Please type a word or a phrase"
-        />
-      </header>
+      <header>Pig Latin Translator</header>
+      <img
+        src="/pigggggy.png"
+        alt="This piggy went to market"
+        // style={{"animation": `pigImageSpin infinite ${pigSpinSpeed}s linear`}}
+        className="pigImage"
+      />
+    
+      <span className="pigLatinOutput">{pigLatinOutput}</span>
+      <input
+        className="englishInput"
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleSpace}
+        placeholder="This little piggy went to market..."
+      />
     </div>
   );
 }

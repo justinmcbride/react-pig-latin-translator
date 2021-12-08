9f6ee79ee8efeeffd517710b5e2980838cf6946a
Justin McBride <justin@mcbride.io>
2021-12-08 16:01:28 -0700

    fix some errors

--- src/App.js
+++ src/App.js
@@ -9,45 +9,33 @@ import _ from "lodash";
 const OINK_SERVER_URL = `http://localhost`;
 const OINK_SERVER_PORT = 5000;
 
-const requestTranslateWord = (wordToTranslate) => {
-  axios.get(`${OINK_SERVER_URL}:${OINK_SERVER_PORT}/translateWord/${wordToTranslate}`)
-  .then(res => {
-    const response = res.data;
-    if (!_.has(response, 'stemChangeIndex') || !_.has(response, 'pigLatinWord')) {
-      console.error(`requestTranslateWord: bad response: response=[${JSON.stringify(response)}]`);
-    }
-    console.log(`requestTranslateWord: wordToTranslate=[${wordToTranslate}] stemChangeIndex=[${response.stemChangeIndex}] pigLatinWord=[${response.pigLatinWord}]`);
-  })
-  .error(err => {
-    console.error(`requestTranslateWord: failure: ${err}`);
-  });
-}
-
-function App() {
-  const [englishInput, setEnglishInput] = useState('');
-  const [pigLatinOutput, setPigLatinOutput] = useState('');
-
+const App = () => {
+  const [englishInput, setEnglishInput] = useState(``);
+  const [pigLatinOutput, setPigLatinOutput] = useState(``);
   const [pigSpinSpeed, setPigSpinSpeed] = useState(20);
+
+  const requestTranslateWord = (wordToTranslate) => {
+    axios.get(`${OINK_SERVER_URL}:${OINK_SERVER_PORT}/translateWord/${wordToTranslate}`)
+    .then(res => {
+      const response = res.data;
+      if (!_.has(response, `stemChangeIndex`) || !_.has(response, `pigLatinWord`)) {
+        console.error(`requestTranslateWord: bad response: response=[${JSON.stringify(response)}]`);
+      }
+      console.log(`requestTranslateWord: wordToTranslate=[${wordToTranslate}] stemChangeIndex=[${response.stemChangeIndex}] pigLatinWord=[${response.pigLatinWord}]`);
+    })
+    .catch(err => {
+      console.error(`requestTranslateWord: failure: ${err}`);
+    });
+  }
 
   const handleChange = (e) => {
     const currentInputText = e.target.value;
     setEnglishInput(currentInputText);
-    if (currentInputText[currentInputText.length-1] === ' ') {
-      // if the last character entered was a space,
-      // we don't want to create a loop, so we return early.
-      return;
-    }
-    console.log(`handleChange: currentInputText=[${currentInputText}]`);
-  }
 
-  const handleSpace = (e) => {
-    if (e.keyCode !== 32) {
-      // we only care about spaces, so just exit if not a space.
-      return;
-    }
+    console.log(`handleChange: currentInputText=[${currentInputText}]`);
 
     // tokenize the words off the space
-    const wordArray = englishInput.split(' ');
+    const wordArray = currentInputText.split(' ');
 
     // create a variable to store the concatenated output
     let allTranslatedWords = '';
@@ -62,16 +50,15 @@ function App() {
       allTranslatedWords += translatedWord + " ";
     }
 
-    console.log(`handleSpace: englishInput=[${englishInput}] allTranslatedWords=[${allTranslatedWords}]`);
+    console.log(`handleChange: allTranslatedWords=[${allTranslatedWords}]`);
     setPigLatinOutput(allTranslatedWords);
-
     setPigSpinSpeed(pigSpinSpeed * .9);
-    console.log(`pigSpinSpeed=[${pigSpinSpeed}]`);
   }
 
   const resetInputField = () => {
-    setEnglishInput("");
-    setPigLatinOutput('');
+    setEnglishInput(``);
+    setPigLatinOutput(``);
+    setPigSpinSpeed(20);
   };
 
   return (
@@ -91,10 +78,9 @@ function App() {
       <span className="pigLatinOutput">{pigLatinOutput}</span>
       <input
         className="englishInput"
-        type="text"
         value={englishInput}
+        type="text"
         onChange={handleChange}
-        onKeyDown={handleSpace}
         placeholder="This little piggy went to market..."
       />
       <button class="button-1" onClick={resetInputField}><span class="text">Reset</span></button>

ec9549696952189fd37a01661494e1b20ae1fcf3
Justin McBride <justin@mcbride.io>
2021-12-09 01:34:15 -0700

    lots of work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css'
 import { useState } from 'react'
-import translator from './translator.js'
 import MicRecorder from 'mic-recorder-to-mp3';
 
 import axios from 'axios';
@@ -10,56 +9,58 @@ import _ from "lodash";
 const OINK_SERVER_URL = `http://localhost`;
 const OINK_SERVER_PORT = 5000;
 
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+
 const App = () => {
   const [englishInput, setEnglishInput] = useState(``);
   const [pigLatinOutput, setPigLatinOutput] = useState(``);
   const [pigSpinSpeed, setPigSpinSpeed] = useState(20);
 
-  const [Mp3Recorder] = useState(new MicRecorder({ bitRate: 128 }));
   const [recordingState, setRecordingState] = useState(false);
 
-  const [translatedWords, setTranslatedWords] = useState([]);
+  const [animatingWords, setAnimatingWords] = useState([]);
 
   const requestTranslateWord = (wordToTranslate) => {
-    axios.get(`${OINK_SERVER_URL}:${OINK_SERVER_PORT}/translateWord/${wordToTranslate}`)
-    .then(res => {
-      const response = res.data;
-      if (!_.has(response, `stemChangeIndex`) || !_.has(response, `pigLatinWord`)) {
-        console.error(`requestTranslateWord: bad response: response=[${JSON.stringify(response)}]`);
-      }
+    console.log(`requestTranslateWord: wordToTranslate=[${wordToTranslate}]`);
 
-      setTranslatedWords([...translatedWords, response.pigLatinWord]);
-      console.log(`requestTranslateWord: wordToTranslate=[${wordToTranslate}] stemChangeIndex=[${response.stemChangeIndex}] pigLatinWord=[${response.pigLatinWord}]`);
-    })
-    .catch(err => {
-      console.error(`requestTranslateWord: failure: ${err}`);
-    });
-  }
+    axios
+      .get(`${OINK_SERVER_URL}:${OINK_SERVER_PORT}/oink/${wordToTranslate}`)
+      .then(res => {
+        const response = res.data;
+        if (!_.has(response, `stemChangeIndex`) || !_.has(response, `pigLatinWord`)) {
+          console.error(`requestTranslateWord: bad response: response=[${JSON.stringify(response)}]`);
+        }
+
+        setPigLatinOutput(`${pigLatinOutput} ${response.pigLatinWord}`);
+
+        setAnimatingWords([...animatingWords, response]);
+        console.log(`requestTranslateWord: wordToTranslate=[${wordToTranslate}] stemChangeIndex=[${response.stemChangeIndex}] pigLatinWord=[${response.pigLatinWord}]`);
+      })
+      .catch(err => {
+        console.error(`requestTranslateWord: failure: ${err}`);
+      })
+    ;
+  };
 
   const handleChange = (e) => {
     const currentInputText = e.target.value;
-    setEnglishInput(currentInputText);
+    const trimmedInput = currentInputText.trimStart();
 
-    console.log(`handleChange: currentInputText=[${currentInputText}]`);
-
-    // tokenize the words off the space
-    const wordArray = currentInputText.split(' ');
-
-    // create a variable to store the concatenated output
-    let allTranslatedWords = '';
-
-    // iterate over every individual word
-    for (const word of wordArray) {
-      // perform the translation of the individual word
-      const translatedWord = translator(word);
-      requestTranslateWord(word);
-
-      // add it to the final output
-      allTranslatedWords += translatedWord + " ";
+    if (trimmedInput.length === 0) {
+      setEnglishInput(``);
+      return;
     }
 
-    console.log(`handleChange: allTranslatedWords=[${allTranslatedWords}]`);
-    setPigLatinOutput(allTranslatedWords);
+    const wholeInputWord = trimmedInput.trimEnd();
+    if (wholeInputWord.length !== trimmedInput.length) {
+      console.log(`Word completed: [${wholeInputWord}]`);
+      setEnglishInput(``);
+
+      requestTranslateWord(wholeInputWord);
+      return;
+    }
+
+    setEnglishInput(trimmedInput);
     setPigSpinSpeed(pigSpinSpeed * .9);
   }
 
@@ -69,45 +70,43 @@ const App = () => {
     setPigSpinSpeed(20);
   };
 
-  const startRecording = () => {
-    setRecordingState(true)
-  }
-
   const startRecord = () => {
+    console.log(`Attempting to start recording`);
     Mp3Recorder
       .start()
       .then(() => {
-        startRecording();
-        console.log("recording state:", {recordingState});
+        setRecordingState(true);
       })
-      .catch((e) => console.error(e))
+      .catch((e) => {
+        console.error(`Failed to start recording`, e);
+      })
     ;
-  }
+  };
 
   const stopRecord = () => {
+    console.log(`Attempting to stop recording`);
     Mp3Recorder
       .stop()
       .getMp3()
       .then(([buffer, blob]) => {
+        setRecordingState(false);
         const file = new File(buffer, 'audio_recording.mp3', {
           type: blob.type,
           lastModified: Date.now()
         });
-        var a = document.createElement("a");
+        const a = document.createElement("a");
         a.href = URL.createObjectURL(file);
         a.download = 'audio_recording.mp3';
         a.click();
-
       }).catch((e) => {
-        alert('We could not retrieve your message');
-        console.log(e);
+        console.error(`Failed to stop recording`, e);
       })
     ;
-  }
+  };
 
   const animatedWords = [];
-  for (const word of translatedWords) {
-    animatedWords.push( <span className="animatedWord">{word}</span> );
+  for (const word of animatingWords) {
+    animatedWords.push( <span className="animatedWord">{word.pigLatinWord}</span> );
   }
 
   return (
@@ -124,10 +123,13 @@ const App = () => {
         alt="Innovation that excites"
         src="/mersive2.png"
       />
-      <span className="pigLatinOutput">{pigLatinOutput}</span>
-      <div id="animationHolder">
-        { animatedWords }
+
+      <div id="animationContainer">
+        {animatedWords}
       </div>
+
+      <div className="pigLatinOutput">{pigLatinOutput}</div>
+      
       <input
         className="englishInput"
         value={englishInput}
@@ -135,7 +137,7 @@ const App = () => {
         onChange={handleChange}
         placeholder="This little piggy went to market..."
       />
-      <button class="button-1" onClick={resetInputField}><span class="text">Reset</span></button>
+      <button className="button-1" onClick={resetInputField}><span class="text">Reset</span></button>
       <button className="button-1" onClick={startRecord} disabled={recordingState}><span className="text">Talk</span></button>
       <button className="button-1" onClick={stopRecord} disabled={!recordingState}><span className="text">Stop Talking</span></button>
     </div>

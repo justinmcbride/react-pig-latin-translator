891725f9339cd743582e542d5d922723499f04ea
Justin McBride <justin@mcbride.io>
2025-02-19 23:21:33 -0700

    working on translator animations

--- src/tabs/NormalTranslator.js
+++ src/tabs/NormalTranslator.js
@@ -1,25 +1,15 @@
-import {useState} from 'react';
+"use client";
 
-import translator from '@/lib/translator';
-import SingleWordInput from '@/components/SingleWordInput';
+import { useState } from "react";
 
-// const OutputAnimation = keyframes`
-//   0% {
-//     color: white;
-//   }
-//   99% {
-//     color:#f34079;
-//   }
+import translator from "@/lib/translator";
+import SingleWordInput from "@/components/SingleWordInput";
+
 //   100%{
 //     opacity: 0;
 //     position: fixed;
 //     z-index: -5;
 //   }
-// `;
-
-// const AnimatedWord = styled.div`
-//   animation: ${OutputAnimation} 4s ease-out forwards;
-// `;
 
 const NormalTranslator = () => {
   const [pigLatinOutput, setPigLatinOutput] = useState(``);
@@ -33,24 +23,32 @@ const NormalTranslator = () => {
     // Because this can be called into a loop, and we want to ensure that ALL
     // updates to the state aren't batched into a single call, use the state
     // updater syntax. https://stackoverflow.com/a/66560573/4493426
-    setPigLatinOutput(previousState => `${previousState} ${translatedWord}`);
-    setAnimatingWords(previousState => [...previousState, translatedWord]);
+    setPigLatinOutput((previousState) => `${previousState} ${translatedWord}`);
+    setAnimatingWords((previousState) => [...previousState, translatedWord]);
 
-    console.log(`handleSubmitWord: inputWord=[${inputWord}] translatedWord=[${translatedWord}]`);
-  }
-
-  const animatedWords = [];
-  for (const word of animatingWords) {
-    animatedWords.push(<div>{word}</div>);
-  }
+    console.log(
+      `handleSubmitWord: inputWord=[${inputWord}] translatedWord=[${translatedWord}]`
+    );
+  };
 
   return (
     <div>
-      <SingleWordInput onSubmitWord={handleSubmitWord}/>
+      <SingleWordInput onSubmitWord={handleSubmitWord} />
       <div className="text-white font-lg">{pigLatinOutput}</div>
-      <div className="h-5">{animatedWords}</div>
+      <div className="h-5">
+        {animatingWords.map((word, index) => {
+          return (
+            <div
+              key={index}
+              className=" text-red-600 p-10 animate-fade animate-infinite animate-duration-[3000ms] animate-ease-linear animate-reverse"
+            >
+              {word}
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default NormalTranslator;
